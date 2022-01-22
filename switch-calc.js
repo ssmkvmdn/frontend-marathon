f3a4ee52192bf96c66b976f3c7503455957a66f6
Black Mirror <33105850+ssmkvmdn@users.noreply.github.com>
2022-01-22 22:41:36 +0300

    Update switch-calc.js

--- switch-calc.js
+++ switch-calc.js
@@ -1,5 +1,5 @@
 function Calc(operation, num1, num2) {
-    const isNotValid = typeof num1 !== 'number' || typeof num2 !== 'number' || isNaN(num1) || isNaN(num2);
+    const isNotValid = typeof num1 !== 'number' || typeof num2 !== 'number';
 
     if (isNotValid) {
         return "Error";
@@ -24,25 +24,3 @@ function Calc(operation, num1, num2) {
 }
 
 console.log(Calc('mult', 15, 2));
-console.log(Calc('sum', 15, 2));
-console.log(Calc('sub', 23, 2));
-console.log(Calc('div', 22, 2));
-console.log(Calc('mult', 0, 23));
-console.log(Calc('pow', 22, 2));
-console.log(Calc('mod', 100, 23));
-
-console.log(Calc('div', 10, 0));
-
-console.log(Calc('sum', 10));
-console.log(Calc('sum', 10, 'lkj'));
-console.log(Calc(10, 2));
-
-console.log(Calc('sum', null, 2));
-console.log(Calc('sum', undefined, 2));
-console.log(Calc('sum', 14, NaN));
-
-console.log(Calc('', 10, 2));
-console.log(Calc(' ', 10, 2));
-console.log(Calc(42, 10, 2));
-console.log(Calc(null, 10, 2));
-console.log(Calc(NaN, 14, 7));
